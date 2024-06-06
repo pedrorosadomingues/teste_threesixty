294cce96af05d4a352bb2cface7f52f99fb5d4b8
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-05 23:27:48 -0300

    ajusts final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ThreeSixtyViewer from "../pages/Test/components/ThreeSixtyViewer";
-import Tour360Viewer from "../pages/Test/components/Tour360Viewer";
+// import Tour360Viewer from "../pages/Test/components/Tour360Viewer";
 
 export default function Home() {
   return (
