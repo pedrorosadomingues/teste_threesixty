673fcc9f84e195be5090cfc0c7a920fb92991e15
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-05 22:10:44 -0300

    ajust color

--- src/pages/Test/components/Tour360Viewer.js
+++ src/pages/Test/components/Tour360Viewer.js
@@ -57,7 +57,7 @@ function Tour360Viewer() {
     renderer.gammaOutput = true;
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = -0.5;
+    renderer.toneMappingExposure = -0.25;
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableZoom = true;
